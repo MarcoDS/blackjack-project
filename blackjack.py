17ec6ca537dd280fd0864bf808d0e1c57afb463c
Marco Fonseca Da Silva <Marco@MacBook-Pro-de-Marco.local>
2018-09-23 19:22:49 +0200

    display hands and add welcome message

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,7 @@
 from random import shuffle
 
 number_of_decks = 6
-number_of_players = 2
+number_of_players = 1
 
 def shuffle_deck(n):    
     initial_deck = [r+s for r in '23456789TJQKA' for s in 'SHDC'] * n
@@ -20,12 +20,21 @@ def initial_deal(game):
         game['player ' + str(i)] = []
         game['player ' + str(i)].append(game['deck'].pop(0))
         game['player ' + str(i)].append(game['deck'].pop(0))
+        print("Player %s your hand is: %s" % (i, game['player ' + str(i)]))
 
+    print("Dealer is showing: %s" % (game['dealer'][0]))
     return game
 
 
 game = {'deck': shuffle_deck(number_of_decks), 'players': number_of_players} #initialize game with a number of decks shuffled and a number of players
 
 
+print("Welcome to BlackJack.")
+print("Everyone starts with 1000$")
+print("Bets are fixed at 50$")
+print("")
 
 initial_deal(game)
+
+#for i in range(1,game['players'] + 1):
+#    decision = input("Player %s will you Hit, Stand or Double ?" % (i))
