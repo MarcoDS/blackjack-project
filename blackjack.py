164910dad5e55ea40e658b2354b9023b93d25dec
MarcoDS <fonsecadasilva.marco@gmail.com>
2018-09-26 18:20:27 +0200

    take correct value for A 11 or 1

--- blackjack.py
+++ blackjack.py
@@ -1,15 +1,11 @@
 from random import shuffle
 
-
 def shuffle_deck(n):    
     initial_deck = [r+s for r in '23456789TJQKA' for s in 'SHDC'] * n
     shuffle(initial_deck)
     return initial_deck
 
-#suits = [s for r,s in mydeck] 
-#ranks = [r for r,s in mydeck] 
-
-def deal(game):
+def deal(game): #make a class
     game['dealer'] = []
     game['dealer'].append(game['deck'].pop(0))
     game['dealer'].append(game['deck'].pop(0))
@@ -23,22 +19,26 @@ def deal(game):
     print("Dealer is showing: %s" % (game['dealer'][0]))
     return game
 
-
-
 def start_game(number_of_decks, number_of_players):
 	game = {'deck': shuffle_deck(number_of_decks), 'players': number_of_players} #initialize game with a number of decks shuffled and a number of players
 	return game
 
 def hand_value(hand):
-	#ranks = ['-A23456789TJQKA'.index(r) for r, s in hand]
 	value = 0
+	ace_count = 0
 	for r,s in hand:
 		if r in '2,3,4,5,6,7,8,9':
 			value += int(r)
 		elif r in 'TJQK':
 			value += 10
 		elif r == 'A':
-			value += 11 
+			value += 11
+			ace_count += 1
+
+	while value > 21 and ace_count > 0:
+		value -= 10
+		ace_count -= 1
+
 	return value
 
 print("Welcome to BlackJack.")
@@ -49,5 +49,6 @@ print("")
 game = start_game(6, 1)
 deal(game)
 
+
 #for i in range(1,game['players'] + 1):
-#    decision = input("Player %s will you Hit, Stand or Double ?" % (i))
+#	decision = input("Player %s will you Hit, Stand or Double ?" % (i))
