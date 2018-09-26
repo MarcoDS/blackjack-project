fb1844ba971d04bbb9f65cb0173877420e93810a
MarcoDS <fonsecadasilva.marco@gmail.com>
2018-09-26 17:30:42 +0200

    add hand_value function - will need to address A value

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,5 @@
 from random import shuffle
 
-number_of_decks = 6
-number_of_players = 1
 
 def shuffle_deck(n):    
     initial_deck = [r+s for r in '23456789TJQKA' for s in 'SHDC'] * n
@@ -11,7 +9,7 @@ def shuffle_deck(n):
 #suits = [s for r,s in mydeck] 
 #ranks = [r for r,s in mydeck] 
 
-def initial_deal(game):
+def deal(game):
     game['dealer'] = []
     game['dealer'].append(game['deck'].pop(0))
     game['dealer'].append(game['deck'].pop(0))
@@ -26,15 +24,30 @@ def initial_deal(game):
     return game
 
 
-game = {'deck': shuffle_deck(number_of_decks), 'players': number_of_players} #initialize game with a number of decks shuffled and a number of players
 
+def start_game(number_of_decks, number_of_players):
+	game = {'deck': shuffle_deck(number_of_decks), 'players': number_of_players} #initialize game with a number of decks shuffled and a number of players
+	return game
+
+def hand_value(hand):
+	#ranks = ['-A23456789TJQKA'.index(r) for r, s in hand]
+	value = 0
+	for r,s in hand:
+		if r in '2,3,4,5,6,7,8,9':
+			value += int(r)
+		elif r in 'TJQK':
+			value += 10
+		elif r == 'A':
+			value += 11 
+	return value
 
 print("Welcome to BlackJack.")
 print("Everyone starts with 1000$")
 print("Bets are fixed at 50$")
 print("")
 
-initial_deal(game)
+game = start_game(6, 1)
+deal(game)
 
 #for i in range(1,game['players'] + 1):
 #    decision = input("Player %s will you Hit, Stand or Double ?" % (i))
