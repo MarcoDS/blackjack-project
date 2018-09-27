497194d6850f0ebdae2b0eb67a129b608175d7ad
MarcoDS <fonsecadasilva.marco@gmail.com>
2018-09-27 15:11:22 +0200

    Create Player class.
    Move function Hit to Player.
    Rework hand_value to compute_value which needs to be called everytime we change player's hand.

--- blackjack.py
+++ blackjack.py
@@ -1,75 +1,66 @@
 from random import shuffle
 
+class Player():
+    def __init__(self):
+        self.hand = []
+        self.hand_value = 0
+
+    def compute_value(self):
+        """Return numeric value of said hand."""
+        self.hand_value = 0
+        ace_count = 0
+        for r, s in self.hand:
+            if r in '2,3,4,5,6,7,8,9':
+                self.hand_value += int(r)
+            elif r in 'TJQK':
+                self.hand_value += 10
+            elif r == 'A':
+                self.hand_value += 11
+                ace_count += 1
+
+        while self.hand_value > 21 and ace_count > 0:
+            self.hand_value -= 10
+            ace_count -= 1
+        
+
+
+    def hit(self, card): 
+        """Add a card from top of the deck to target hand."""
+        self.hand.append(card)
+        self.compute_value()
+
 class Game():
+    def __init__(self, number_of_decks, number_of_players):
+        self.number_of_decks = number_of_decks
+        self.number_of_players = number_of_players
+        self.deck = []
+        self.players = [Player() for i in range(number_of_players + 1)]  # additional player to account for the dealer
 
-	def __init__(self, number_of_decks, number_of_players):
-		self.number_of_decks = number_of_decks
-		self.number_of_players = number_of_players
-		self.current_deck = []
-		self.hands = {}
+    def deal(self):
+        """Set up each round by creating starting hands for dealer and each player."""
+        for p in list(range(len(self.players))) * 2:
+            self.players[p].hit(self.deck.pop(0))
 
-	def deal(self): 
-		#Sets up each round by creating starting hands for dealer and each player
-	    self.hands['dealer'] = []
-	    self.hit('dealer')
-	    self.hit('dealer')
+    def print_deal(self): 
+        """Print initial deal output."""
+        for p in range(1, len(self.players)):
+            print("Player %s your hand is: %s" % (p, self.players[p].hand))
 
-	    for i in range(self.number_of_players):
-	        self.hands['player ' + str(i)] = []
-	        self.hit(str(i))
-	        self.hit(str(i))
-	   
-	    return self.hands
+        print("Dealer is showing: %s" % (self.players[0].hand))
 
-	def hit(self, target): 
-		#Adds a card from top of the deck to target hand
-		if target == 'dealer':
-			self.hands[target].append(self.current_deck.pop(0))
-		else:
-			self.hands['player ' + str(target)].append(self.current_deck.pop(0))
-		return self.hands
-
-	def print_deal(self): 
-		#Prints initial deal output
-		for i in range(self.number_of_players):
-			print("Player %s your hand is: %s" % ((i+1), self.hands['player ' + str(i)]))
-
-		print("Dealer is showing: %s" % (self.hands['dealer'][0]))
-
-	def shuffle_deck(self):    
-		#Shuffles total deck to create a new one
-	    self.current_deck = [r+s for r in '23456789TJQKA' for s in 'SHDC'] * self.number_of_decks
-	    shuffle(self.current_deck)
-	    return self.current_deck
-
-def hand_value(hand):
-	#Returns numeric value of said hand
-	value = 0
-	ace_count = 0
-	for r,s in hand:
-		if r in '2,3,4,5,6,7,8,9':
-			value += int(r)
-		elif r in 'TJQK':
-			value += 10
-		elif r == 'A':
-			value += 11
-			ace_count += 1
-
-	while value > 21 and ace_count > 0:
-		value -= 10
-		ace_count -= 1
-
-	return value
+    def shuffle_deck(self):    
+        """Shuffle total deck to create a new one."""
+        self.deck = [r+s for r in '23456789TJQKA' for s in 'SHDC'] * self.number_of_decks
+        shuffle(self.deck)
 
 print("Welcome to BlackJack.")
 print("Everyone starts with 1000$")
 print("Bets are fixed at 50$")
 print("")
 
-current_game = Game(6, 1)
-current_game.shuffle_deck()
-current_game.deal()
-current_game.print_deal()
+game = Game(6, 1)
+game.shuffle_deck()
+game.deal()
+game.print_deal()
 
 
-#	decision = input("Player %s will you Hit, Stand or Double ?" % (i))
