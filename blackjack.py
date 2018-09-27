7be1f6c8773e9a00691f7833e8f87ac6df818b99
MarcoDS <fonsecadasilva.marco@gmail.com>
2018-09-27 12:09:29 +0200

    clean player iterating

--- blackjack.py
+++ blackjack.py
@@ -1,44 +1,49 @@
 from random import shuffle
 
 class Game():
-	
+
 	def __init__(self, number_of_decks, number_of_players):
 		self.number_of_decks = number_of_decks
 		self.number_of_players = number_of_players
 		self.current_deck = []
 		self.hands = {}
 
-	def deal(self): #make a class
+	def deal(self): 
+		#Sets up each round by creating starting hands for dealer and each player
 	    self.hands['dealer'] = []
 	    self.hit('dealer')
 	    self.hit('dealer')
 
-	    for i in range(1,self.number_of_players + 1):
+	    for i in range(self.number_of_players):
 	        self.hands['player ' + str(i)] = []
 	        self.hit(str(i))
 	        self.hit(str(i))
 	   
 	    return self.hands
 
-	def hit(self, target): #target should be dealer or player id
+	def hit(self, target): 
+		#Adds a card from top of the deck to target hand
 		if target == 'dealer':
 			self.hands[target].append(self.current_deck.pop(0))
 		else:
 			self.hands['player ' + str(target)].append(self.current_deck.pop(0))
 		return self.hands
 
-	def print_deal(self): #i see i can replace the default print / look up the syntax
-		for i in range(1,self.number_of_players + 1):
-			print("Player %s your hand is: %s" % (i, self.hands['player ' + str(i)]))
+	def print_deal(self): 
+		#Prints initial deal output
+		for i in range(self.number_of_players):
+			print("Player %s your hand is: %s" % ((i+1), self.hands['player ' + str(i)]))
 
 		print("Dealer is showing: %s" % (self.hands['dealer'][0]))
 
 	def shuffle_deck(self):    
+		#Shuffles total deck to create a new one
 	    self.current_deck = [r+s for r in '23456789TJQKA' for s in 'SHDC'] * self.number_of_decks
 	    shuffle(self.current_deck)
 	    return self.current_deck
 
 def hand_value(hand):
+	#Returns numeric value of said hand
 	value = 0
 	ace_count = 0
 	for r,s in hand:
